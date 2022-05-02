492d7def639ff19e0f1a65c54b797b05f42aefcd
Srijan Shovit <srijanshovit@gmail.com>
2022-05-02 12:06:03 +0530

    test audio

--- src/NewJFrame2.java
+++ src/NewJFrame2.java
@@ -365,7 +365,7 @@ public class NewJFrame2 extends javax.swing.JFrame {
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
         try{
-                String path="C:\\Users\\hp\\Documents\\piano\\generate2.wav";
+                String path="E:\\JavaSwing\\Mess-Fee-Calculator\\src\\audio\\generate2.wav";
                 AudioInputStream audio= AudioSystem.getAudioInputStream(new File(path));
                 Clip player = AudioSystem.getClip();
                 player.open(audio);
@@ -417,7 +417,7 @@ public class NewJFrame2 extends javax.swing.JFrame {
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
         try{
-                String path="C:\\Users\\hp\\Documents\\piano\\reset2.wav";
+                String path="E:\\JavaSwing\\Mess-Fee-Calculator\\src\\audio\\reset2.wav";
                 AudioInputStream audio= AudioSystem.getAudioInputStream(new File(path));
                 Clip player = AudioSystem.getClip();
                 player.open(audio);
@@ -446,7 +446,7 @@ public class NewJFrame2 extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         try{
-                String path="C:\\Users\\hp\\Documents\\piano\\print2.wav";
+                String path="E:\\JavaSwing\\Mess-Fee-Calculator\\src\\audio\\print2.wav";
                 AudioInputStream audio= AudioSystem.getAudioInputStream(new File(path));
                 Clip player = AudioSystem.getClip();
                 player.open(audio);
@@ -465,7 +465,7 @@ public class NewJFrame2 extends javax.swing.JFrame {
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         // TODO add your handling code here:
         try{
-                String path="C:\\Users\\hp\\Documents\\piano\\about2.wav";
+                String path="E:\\JavaSwing\\Mess-Fee-Calculator\\src\\audio\\about2.wav";
                 AudioInputStream audio= AudioSystem.getAudioInputStream(new File(path));
                 Clip player = AudioSystem.getClip();
                 player.open(audio);
